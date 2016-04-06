4aff0c1e6518ede63dfbacf4ef4c4a9ca862c83f
Ben McFadden <ben@forgeapps.com>
2016-04-06 11:33:03 -0700

    Add a JSON response, because that's more useful

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
-  post '/' => 'status#update'
+  # Rails really doesn't like having a format (ex, JSON) on the root route
+  # so I've added a duplicate /status route to make the JSON API simpler.
+  get '/status' => "status#index"
+  post '/status' => "status#update"
   
+  post '/' => 'status#update'  
   root 'status#index'
 end
